c3819572c3d79b926a8285e8b70988c00b29dde6
vinay-chindam <vinnuhary@gmail.com>
2024-04-20 00:56:30 +0530

    succwssfully commmitting cart

---
+++ src/components/Spinner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./spinner.css"
+
+export default function Spinner() {
+  return (
+    <div>
+        <div className='custom-loader'>
+
+
+        </div>
+    </div>
+  )
+}
